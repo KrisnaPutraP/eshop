3d55ec8a99b661d0a68668da8717a167e03b9ced
KrisnaPutraP <krisna.putra@ui.ac.id>
2025-02-09 23:03:34 +0700

    [FEAT]: Adding method interface and implementation for deleting a product inside the service package

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -42,4 +42,9 @@ public class ProductServiceImpl implements ProductService {
     public Product update(Product product) {
         return productRepository.update(product);
     }
+
+    @Override
+    public void deleteById(String productId) {
+        productRepository.deleteById(productId);
+    }
 }
