12e577e818a629e6f9c2cec5bca9a35a8b64275e
KrisnaPutraP <krisna.putra@ui.ac.id>
2025-03-06 09:46:44 +0700

    fix: Fixing payment repository test

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -1,6 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
